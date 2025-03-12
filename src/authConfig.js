78172a767cbff7eff685a47562803bbf810156c7
mark <markhughesster@gmail.com>
2025-03-12 10:21:47 +0000

    Relates #13. Edited import path

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,9 @@ export const msalConfig = {
     auth: {
         clientId: "662d94ed-38d5-494e-a046-eabdd43d6584",
         authority: "https://login.microsoftonline.com/b7cfda6d-56ed-46ea-b26f-c84e3b9bd53c",
-        redirectUri: "http://localhost:3000"
+        // redirectUri: "http://localhost:3000"
+        redirectUri: process.env.REACT_APP_REDIRECT_URI
+
     },
     cache: {
         cacheLocation: "sessionStorage",
