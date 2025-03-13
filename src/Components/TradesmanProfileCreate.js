cb3b23992eb731fe8c6317f9a0ae80a438748afd
mark <markhughesster@gmail.com>
2025-03-13 12:03:53 +0000

    #12. Changed API Import

--- src/Components/TradesmanProfileCreate.js
+++ src/Components/TradesmanProfileCreate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import API from '../Api';
 
 function TradesmanProfileCreate() {
     const [tradesmanDetails, setTradesmanDetails] = useState({
